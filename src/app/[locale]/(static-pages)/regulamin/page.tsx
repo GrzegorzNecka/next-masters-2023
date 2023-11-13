106ecdfbd09f54e2d728b979ec8ae7b5d0e947e0
GrzegorzNecka <strzypior@gmail.com>
2023-11-13 21:30:57 +0100

    lesson 2.2

--- src/app/[locale]/(static-pages)/regulamin/page.tsx
+++ src/app/[locale]/(static-pages)/regulamin/page.tsx
@@ -1,14 +1,14 @@
-export default function PolitykaPrywatnosciPage() {
+export default function Regulamin() {
 	return (
-		<div>
+		<>
 			<h1 className="mb-4 text-4xl font-bold tracking-tight text-gray-900 md:text-3xl lg:text-4xl ">
-				Polityka Prywatnosci
+				Regulamin
 			</h1>
 			<p className="mb-4 text-lg text-gray-700">
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, dolorem necessitatibus
 				mollitia incidunt fugiat porro nam, nobis earum distinctio itaque nemo qui eveniet id, non
 				magnam! Quis cupiditate exercitationem sit?
 			</p>
-		</div>
+		</>
 	);
 }
