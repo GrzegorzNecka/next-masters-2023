c2933a49e5d6bf53dfab2f5c80e250ea161fc4fc
GrzegorzNecka <global-strzypior@gmail.com>
2024-01-24 14:30:22 +0100

    blur

--- src/ui/atoms/ProductListItemCoverImage.tsx
+++ src/ui/atoms/ProductListItemCoverImage.tsx
@@ -9,6 +9,8 @@ export const ProductListItemCoverImage = ({ image, alt }: ProductListItemCoverIm
 	return (
 		<div className="aspect-square overflow-hidden rounded-md border border-stone-200 bg-stone-300">
 			<Image
+				placeholder="blur"
+				blurDataURL={image.url}
 				width={300}
 				height={300}
 				className="h-full w-full  object-cover object-center p-3 transition-transform hover:scale-105"
