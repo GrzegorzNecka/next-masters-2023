42aa646e311da0abd859629c1b66be3f86a2fc0d
GrzegorzNecka <global-strzypior@gmail.com>
2024-02-19 15:23:24 +0100

    4.1: Server Actions 1

--- src/app/product/[productSlug]/page.tsx
+++ src/app/product/[productSlug]/page.tsx
@@ -28,21 +28,31 @@ export default async function SingleProductPage({
 		return <p>produkt chwilowo niedostępny</p>;
 	}
 
+	async function addToCartAction(formData: FormData) {
+		"use server";
+		console.log(formData);
+	}
+
 	return (
 		<>
 			<section className="flex gap-10">
 				<ProductSingleCoverImage image={product.images.at(0)} alt={product.name} />
 				<div>
 					<ProductSingleDescription product={product} />
-					<Suspense fallback={"ładownienie"}>
-						<ProductVariantsList searchParams={searchParams} product={product} />
-					</Suspense>
+					<ProductVariantsList searchParams={searchParams} product={product} />
+					<p className="ml-1 text-sm font-semibold text-slate-500">in stock</p>
+					<form action={addToCartAction}>
+						<input type="hidden" name="productId" value={product.id} />
+						<button type="submit" className="rounded-sm border bg-slate-200 px-6 py-2 shadow-sm">
+							Add to cart
+						</button>
+					</form>
 				</div>
 			</section>
 
 			<aside>
 				<hr className="my-10" />
-				<Typography className="mb-10 text-xl font-semibold" isUppercase={true} as="h2">
+				<Typography className="mb-10 mt-4 text-xl font-semibold" isUppercase={true} as="h2">
 					sugerowane
 				</Typography>
 
